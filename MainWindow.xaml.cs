515cc80f6ea10a22739231dc426d5f9acb040a92
yanouyehiel <yanou.yehiel@yahoo.com>
2023-12-27 17:03:11 +0100

    Ajoutez des fichiers projet.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
+namespace Gestion_Annur
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnectClick(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && !string.IsNullOrEmpty(txtPassword.Password))
+            {
+                bool result = this.ConnectUser(txtEmail.Text, txtPassword.Password);
+                if (result)
+                {
+                    MessageBox.Show("Identifiants corrects");
+                }
+                else
+                {
+                    MessageBox.Show("Identifiants incorrects");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez remplir tous les champs !");
+            }
+        }
+
+        public bool ConnectUser(string matricule, string password)
+        {
+            string connectionString = "server=localhost;database=annur;user=root;password=";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM employes WHERE matricule = @Mat AND password = @Pass";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Mat", matricule);
+                command.Parameters.AddWithValue("@Pass", password);
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    
+                    return true;
+                }
+                else
+                {
+                    
+                    return false;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine("Erreur de connexion à la base de données: " + ex.Message);
+                return false;
+            }
+        }
+
+
+        private void textEmail_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtEmail.Focus();
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtEmail.Text) && txtEmail.Text.Length > 0)
+            {
+                textEmail.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textEmail.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void textPassword_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txtPassword.Focus();
+        }
+
+        private void txtPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtPassword.Password) && txtPassword.Password.Length > 0)
+            {
+                textPassword.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                textPassword.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void Image_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
